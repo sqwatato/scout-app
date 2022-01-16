83a4b1014debdeaecfb500498d0d02520827e058
Paarth Mishra <pmshot607@gmail.com>
2022-01-15 17:02:59 -0800

    Updated auton to 2022 fields

--- components/Auton.tsx
+++ components/Auton.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useRef } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import Header from "./Header";
-import { useAuton, usePreGame } from "../Stores";
+import {db} from '../firebase';
 import BottomSheet from "@gorhom/bottom-sheet";
 import QRCodeBottomSheet from "./QRCode";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, Alert } from "react-native";
 import { Text, Toggle } from "@ui-kitten/components";
 import Counter from "./Counter";
 import {
@@ -11,97 +11,84 @@ import {
   NavigationState,
   NavigationParams,
 } from "react-navigation";
-
 interface AutonProps {
   navigation: any; // NavigationScreenProp<NavigationState, NavigationParams>;
 }
 
 const Auton: FC<AutonProps> = ({ navigation }) => {
-  const teams = usePreGame((state) => state.teams);
+  /*const teams = usePreGame((state) => state.teams);
   const alliance = usePreGame((state) => state.alliance);
-  const regional = usePreGame((state) => state.regional);
+  const regional = usePreGame((state) => state.regional);*/
+  const [autonFields, setAutonFields] = useState<any[]>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const fields = await db.collection('years').doc('2022').collection('scouting').doc('auton').get();
+      // setAutonFields(fields.data());
+      setAutonFields(Object.keys(fields.data() || {}).map(field => ({name: field, type: (fields.data() || {})[field]})))
+      // Alert.alert(JSON.stringify(fields.data()));
+    }
+    fetchData();
+  }, [])
 
-  const preloads = useAuton((state) => state.preloads);
-  const setPreloads = useAuton((state) => state.setPreloads);
-
-  const initLineCrosses = useAuton((state) => state.initLineCrosses);
-  const setInitLineCrosses = useAuton((state) => state.setInitLineCrosses);
-
-  const autonBottom = useAuton((state) => state.autonBottom);
-  const setAutonBottom = useAuton((state) => state.setAutonBottom);
-  const autonUpper = useAuton((state) => state.autonUpper);
-  const setAutonUpper = useAuton((state) => state.setAutonUpper);
-  const autonInner = useAuton((state) => state.autonInner);
-  const setAutonInner = useAuton((state) => state.setAutonInner);
-
-  const autonBottomMissed = useAuton((state) => state.autonBottomMissed);
-  const setAutonBottomMissed = useAuton((state) => state.setAutonBottomMissed);
-  const autonUpperMissed = useAuton((state) => state.autonUpperMissed);
-  const setAutonUpperMissed = useAuton((state) => state.setAutonUpperMissed);
+  const renderFields = () => {
+    Alert.alert("Called");
+    for(let key in autonFields) {
+      if(autonFields.hasOwnProperty(key)) {
+        return (
+          <View>
+            {key + " -> " + autonFields[key]}
+          </View>
+        )
+      }
+    }
+  }
 
   const sheetRef = useRef<BottomSheet>(null);
   return (
     <>
-      <Header
-        matchInfo={{ teams, alliance, regional }}
-        title={"Auton"}
-        toggleQRCode={() => sheetRef.current?.snapTo(1)}
-      />
-      <ScrollView
-        contentContainerStyle={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "10%",
-        }}
-        keyboardDismissMode="on-drag"
+      <View
+        // contentContainerStyle={{
+        //   display: "flex",
+        //   flexDirection: "column",
+        //   padding: "10%",
+        //   // backgroundColor: 'red'
+        // }}
+        // keyboardDismissMode="on-drag"
+        style={{width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}
       >
-        <View>
-          <Text category="h4">Auton Made Shots</Text>
-          <Counter
-            name="Bottom Port"
-            onChange={(val) => setAutonBottom(val)}
-            value={autonBottom}
-          />
-          <Counter
-            name="Upper Port"
-            onChange={(val) => setAutonUpper(val)}
-            value={autonUpper}
-          />
-          <Counter
-            name="Inner Port"
-            onChange={(val) => setAutonInner(val)}
-            value={autonInner}
-          />
-        </View>
-        <View style={{ marginTop: "3%" }}>
-          <Text category="h4">Auton Missed Shots</Text>
-          <Counter
-            name="Bottom Port"
-            onChange={(val) => setAutonBottomMissed(val)}
-            value={autonBottomMissed}
-          />
-          <Counter
-            name="Upper Port"
-            onChange={(val) => setAutonUpperMissed(val)}
-            value={autonUpperMissed}
-          />
-        </View>
-        <Toggle
-          checked={initLineCrosses}
-          onChange={setInitLineCrosses}
-          style={{ marginTop: "3%" }}
-        >
-          Crossed Init Line
-        </Toggle>
-        <Counter
-          name="Preloads"
-          onChange={(val) => setPreloads(val)}
-          value={preloads}
-        />
-      </ScrollView>
+        <Text>{autonFields?.length}</Text>
+        {/* {autonFields?.map(field => <Text>{field['name'] + " -> " + field['type']}</Text>)}  */}
+        {autonFields?.map((field) => {
+          if(field['type'] == 'counter') {
+            return(
+              <Counter
+              name={field['name']}
+              onChange={() => Alert.alert("Change")}
+              value={0}/>
+            )
+          }
+          else if(field['type']=='boolean'){
+            return(
+              <Toggle checked = {false} onChange={() => Alert.alert("Stuff")} style = {{marginTop: "3%"}}>{field['name']}</Toggle>
+            )
+          }
+        })}
+      </View>
       <QRCodeBottomSheet sheetRef={sheetRef} navigation={navigation} />
     </>
   );
 };
 
 export default Auton;
+/*
+
+          if(field['type']==="counter"){
+            <Counter
+            name={field['name']}
+            onChange={() => Alert.alert("Change")}
+            value={0}/>
+          }
+          if(field['type'] == 'boolean') {
+            <Toggle checked = {false} onChange={() => Alert.alert("Stuff")} style = {{marginTop: "3%"}}/>
+          }
+        })*/
---
+++ firebase.ts
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVYgLi7CnBXVAo4aN0qFbRGFSozXiltHM",
+  authDomain: "mvrt115-scout.firebaseapp.com",
+  projectId: "mvrt115-scout",
+  storageBucket: "mvrt115-scout.appspot.com",
+  messagingSenderId: "161834372741",
+  appId: "1:161834372741:web:21f42a585166e7479323ec"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+//const db = app.firestore();
+export const db = app.firestore();
+export default firebase;
