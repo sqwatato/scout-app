328cb639efc2e71ecb38f6d11eea477529e05083
Rohith <rohith.krithivasan@gmail.com>
2022-02-16 19:46:08 -0800

    changed the color scheme of the login buttons

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -2,12 +2,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationState, RouteProp } from "@react-navigation/native";
 import React, { FC, useEffect, useRef, useState } from "react";
 import { NavigationScreenProp, NavigationParams } from "react-navigation";
-import { Alert
-    , View
-    , Text
-    , StyleSheet, KeyboardAvoidingView
-    , TextInput
-    , TouchableOpacity } from "react-native";
+import {
+  Alert,
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { db, auth } from "../firebase";
 
 const Tab = createBottomTabNavigator();
@@ -24,121 +27,119 @@ interface MatchProps {
 }
 
 const Login: FC<MatchProps> = ({ route, navigation }) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-
-    const handleLogin = () => {
-        auth.
-        signInWithEmailAndPassword(email, password).then(
-            () => { Alert.alert("Sign in successful"); navigation.goBack(); }
-        ).catch((error) => {
-            if (error.code === 'auth/invalid-email')
-                Alert.alert('You did not enter a valid Email');
-            else if (error.code === 'auth/user-not-found')
-                Alert.alert('This user could not be found');
-            else if (error.code === 'auth/wrong-password')
-                Alert.alert('Wrong password');
-        });
-    }
-    return(
-        <>
-            <View>
-                <TouchableOpacity
-                    onPress = {() => navigation.goBack()}
-                    style = {{
-                        marginTop: 20,
-                        marginLeft: 20,
-                        marginVertical: 20,
-                    }}
-                >
-                    <Text style = {{
-                        fontWeight: '800',
-                        fontSize: 24
-                    }}>
-                        ←
-                    </Text>
-                </TouchableOpacity>
-            </View>
-            <KeyboardAvoidingView 
-                style = { styles.container }
-                behavior='padding'
-            >
-                <View style = { styles.inputContainer }>
-                    <TextInput 
-                        placeholder = "Enter email"
-                        value = {email.toLowerCase()}
-                        onChangeText={text => setEmail(text.toLowerCase())}
-                        style = {styles.input}
-                        autoCorrect
-                    />
-                    <TextInput 
-                        placeholder = "Password"
-                        value = {password}
-                        onChangeText={text => setPassword(text)}
-                        style = {styles.input}
-                        secureTextEntry
-                    />
-                </View>
-                <View style = { styles.buttonContainer }>
-                    <TouchableOpacity
-                        onPress={() => handleLogin()}
-                        style = { styles.button }
-                    >
-                        <Text style = { styles.buttonText }>Login</Text>
-                    </TouchableOpacity>
-                </View>
-            </KeyboardAvoidingView>
-        </>
-    )
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert("Sign in successful");
+        navigation.goBack();
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-email")
+          Alert.alert("You did not enter a valid Email");
+        else if (error.code === "auth/user-not-found")
+          Alert.alert("This user could not be found");
+        else if (error.code === "auth/wrong-password")
+          Alert.alert("Wrong password");
+      });
+  };
+  return (
+    <>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{
+            marginTop: 20,
+            marginLeft: 20,
+            marginVertical: 20,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "800",
+              fontSize: 24,
+            }}
+          >
+            ←
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Enter email"
+            value={email.toLowerCase()}
+            onChangeText={(text) => setEmail(text.toLowerCase())}
+            style={styles.input}
+            autoCorrect
+          />
+          <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            style={styles.input}
+            secureTextEntry
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => handleLogin()} style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
 export default Login;
 
 const styles = StyleSheet.create({
-    container : {
-        display: "flex",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    inputContainer : {
-        width : '80%',
-    }, 
-    input : {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
-    buttonContainer : {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40,
-    },
-    button : {
-        backgroundColor: "#0782F9",
-        width: '100%',
-        padding: 15, 
-        borderRadius: 10,
-        alignItems: 'center',
-    },
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: "#0782F9",
-        borderWidth: 2,
-    }, 
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-    buttonOutlineText : {
-        color: "#0782F9",
-        fontWeight: '700',
-        fontSize: 16,
-    }
+  container: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#2f064b",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#2f064b",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+  },
 });
